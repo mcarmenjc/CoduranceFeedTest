1dc1fa001c4d7563d94038366b6dd29ed3e1311b
mcarmenjc <mcarmen.jimenez.campos@gmail.com>
2015-09-14 10:10:07 +0100

    Added user test class

---
+++ CoduranceTwitterTests/Entities/UserTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using NUnit.Framework;
+using CoduranceTwitter.Entities;
+using System.Collections.Generic;
+
+namespace CoduranceTwitterTests
+{
+	[TestFixture ()]
+	public class UserTest
+	{
+		[Test()]
+		public void UserNameShouldBeAddedToFollowingsUserList ()
+		{
+			User user = new User ()
+			{ 
+				Name = "mcarmen",
+				Followings = new List<string>(),
+				Messages = new List<Message>()
+			};
+			User followingUser = new User ()
+			{ 
+				Name = "rob",
+				Followings = new List<string>(),
+				Messages = new List<Message>()
+			};
+			user.Follow (followingUser);
+			Assert.AreEqual (1, user.Followings.Count);
+		}
+	}
+}
+
